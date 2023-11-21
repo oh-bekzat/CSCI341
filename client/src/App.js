64399ff683eb4d70679548775af35dd75a523cca
oh-bekzat <111849517+oh-bekzat@users.noreply.github.com>
2023-11-21 07:40:11 +0600

    need to connect frontend to backend yet

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const API_URL = 'http://your-api-url'; // Replace with your actual API URL
+
+const App = () => {
+  const [userData, setUserData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [token, setToken] = useState('');
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setUserData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleRegistration = async () => {
+    try {
+      await axios.post(`${API_URL}/users/create`, userData);
+      console.log('User registered successfully!');
+    } catch (error) {
+      console.error('Error registering user:', error);
+    }
+  };
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post(`${API_URL}/login`, {
+        email: userData.email,
+        password: userData.password,
+      });
+
+      const { token } = response.data;
+      setToken(token);
+
+      console.log('User logged in successfully!');
+      console.log('Token:', token);
+    } catch (error) {
+      console.error('Error logging in:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Registration</h2>
+      <form>
+        {/* Render registration form fields here */}
+        <button type="button" onClick={handleRegistration}>Register</button>
+      </form>
+
+      <h2>Login</h2>
+      <form>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={userData.email}
+          onChange={handleInputChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={userData.password}
+          onChange={handleInputChange}
+        />
+        <button type="button" onClick={handleLogin}>Login</button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
---
+++ server/middleware/authenticate.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken')
+
+const authenticateToken = (req, res, next) => {
+    const token = req.header('Authorization')
+    if (!token) return res.status(401).json({ error: 'Unauthorized' })
+
+    try {
+        const user = jwt.verify(token, 'meiram_is_the_coolest')
+        req.user = user
+        next()
+    } catch (error) {
+        return res.status(403).json({ error: 'Invalid token' })
+    }
+}
+
+module.exports = authenticateToken
